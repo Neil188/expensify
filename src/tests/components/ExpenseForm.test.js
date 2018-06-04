28031626252db4096768c168bb2da98bde3ebd47
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-06-04 11:11:00 +0100

    Feat: ExpenseForm test - add mocks

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -77,4 +79,50 @@ test('should not set amount if invalid input', () => {
         target: { value },
     });
     expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit prop for valid form submission', () => {
+    // onSubmit mock
+    const onSubmitSpy = jest.fn();
+
+    const wrapper = shallow(
+        <ExpenseForm
+            expense={expenses[0]}
+            onSubmit={onSubmitSpy}
+        />);
+    // submit form
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {},
+    });
+
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        // description: expenses[0].description,
+        // amount: expenses[0].amount,
+        // note: expenses[0].note,
+        // createdAt: expenses[0].createdAt,
+        ...expenses[0],
+        id: undefined,
+    });
+});
+
+test('should set new date on date changed', () => {
+    const now = moment();
+    const wrapper = shallow(
+        <ExpenseForm
+            onSubmit={() => {}}
+        />);
+    wrapper.find(SingleDatePicker).prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('should set calendarFocused on focus change', () => {
+    const focused = true;
+    const wrapper = shallow(
+        <ExpenseForm
+            onSubmit={() => {}}
+        />);
+    expect(wrapper.state('calendarFocused')).toEqual(!focused);
+    wrapper.find(SingleDatePicker).prop('onFocusChange')({ focused });
+    expect(wrapper.state('calendarFocused')).toEqual(focused);
 });
