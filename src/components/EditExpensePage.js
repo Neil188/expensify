ecc7d76bd61d87796c2caaea8d50cc8c94adfdd1
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-06-04 21:01:40 +0100

    Refactor: EditExpensePage
    Convert to Class component
    Export base component (without Redux connect)
    add mapDispatchToProps

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = ({ expense, dispatch, history }) => (
-    <div>
-        <ExpenseForm
-            expense={expense}
-            onSubmit={ (update) => {
-                dispatch(editExpense(expense.id, update));
-                history.push('/');
-            }}
-        />
-        <button
-            onClick={ () => {
-                dispatch(removeExpense( { id : expense.id } ));
-                history.push('/');
-            }}
-        >Remove</button>
-    </div>
-);
+export class EditExpensePage extends Component {
+    onSubmit = (expense) => {
+        this.props.editExpense(this.props.expense.id, expense);
+        this.props.history.push('/');
+    }
+    onClick = (expense) => {
+        this.props.removeExpense( { id: this.props.expense.id } );
+        this.props.history.push('/');
+    }
+
+    render() {
+        const { expense } = this.props;
+        return (
+            <div>
+                <ExpenseForm
+                    expense={expense}
+                    onSubmit={ this.onSubmit }
+                />
+                <button
+                    onClick={ this.onClick }
+                >Remove</button>
+            </div>
+        )}
+}
 
 EditExpensePage.propTypes = {
-    dispatch: PropTypes.func.isRequired,
+    // dispatch: PropTypes.func.isRequired,
+    editExpense: PropTypes.func.isRequired,
+    removeExpense: PropTypes.func.isRequired,
     expense: PropTypes.shape({
         id: PropTypes.string.isRequired,
     }).isRequired,
@@ -32,10 +41,15 @@ EditExpensePage.propTypes = {
     }).isRequired,
 }
 
+const mapDispatchToProps = (dispatch) => ({
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (data) => dispatch(removeExpense( data )),
+});
+
 const mapStateToProps = (state, props) =>
     ({
         expense: state.expenses.find( (expense) =>
             expense.id === props.match.params.id),
     });
 
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
