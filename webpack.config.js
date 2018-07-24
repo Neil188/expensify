2491860628e9bf26f7a7958409154b25845aa639
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-07-24 20:56:13 +0100

    Feat: add env variables
    read .env files and pass through to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+const webpack = require('webpack');
 const path = require('path');
+const dotenv = require('dotenv');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+    dotenv.config({
+        path: '.env.test',
+    })
+} else if (process.env.NODE_ENV === 'development') {
+    dotenv.config({
+        path: '.env.development',
+    })
+}
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -45,6 +59,20 @@ module.exports = (env) => {
                 localesToKeep: ['en-gb'],
             }),
             CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY':
+                    JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN':
+                    JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL':
+                    JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID':
+                    JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET':
+                    JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID':
+                    JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            }),
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
