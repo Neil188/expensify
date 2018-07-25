3c933fdb4c2d358cf2965504f63f0a7853e49cff
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-07-25 19:08:14 +0100

    Feat: add set expenses action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -36,3 +36,29 @@ export const editExpense = ( id, updates) => ({
     id,
     updates,
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses,
+});
+
+export const startSetExpenses = () =>
+    (dispatch) =>
+        database.ref('expenses')
+            .once('value')
+            .then( snapshot => {
+                const expenses = [];
+
+                snapshot.forEach( childSnapShot => {
+                    expenses.push({
+                        id: childSnapShot.key,
+                        ...childSnapShot.val(),
+                    })
+                })
+                return expenses;
+            })
+            .then( expenses =>
+                dispatch(setExpenses( expenses ))
+            );
+
