3432786c95fd74c0921c4401d0c8895e18d5ef9b
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-07-27 21:21:41 +0100

    Test: seperate click tests

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -3,9 +3,13 @@ import { shallow } from 'enzyme';
 import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
+    const wrapper = shallow(<Header  startLogOutProcess={ () => {} } />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startlogout on button click', () => {
     const startLogOut = jest.fn();
     const wrapper = shallow(<Header  startLogOutProcess={startLogOut} />);
-    expect(wrapper).toMatchSnapshot();
     wrapper.find('button').simulate('click');
     expect(startLogOut).toHaveBeenCalled();
 });
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -3,9 +3,13 @@ import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
 test('should render Login page correctly', () => {
+    const wrapper = shallow(<LoginPage startLoginProcess={ () => {} } />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startlogin on button click', () => {
     const startLogin = jest.fn();
     const wrapper = shallow(<LoginPage startLoginProcess={startLogin} />);
-    expect(wrapper).toMatchSnapshot();
     wrapper.find('button').simulate('click');
     expect(startLogin).toHaveBeenCalled();
 });
