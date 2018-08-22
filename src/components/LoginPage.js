aecffad0f05948f36cf475ae684544a0bfc32584
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-08-22 21:11:40 +0100

    Feat: style login screen

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLoginProcess }) => (
-    <div>
-        <button onClick={startLoginProcess} >Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify App</h1>
+            <p>It&apos;s time to get your expenses under control.</p>
+            <button onClick={startLoginProcess} >Login</button>
+        </div>
     </div>
 )
 
