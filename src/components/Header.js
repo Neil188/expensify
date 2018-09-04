c3fec89104f652d33534f28bdd044c2d144f6e0a
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-09-04 20:30:28 +0100

    Feat: Style header navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogOut } from '../actions/auth';
 
 export const Header = ({ startLogOutProcess }) => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink
-            to='/dashboard'
-            activeClassName='is-active'
-            exact
-        >
-            Dashboard
-        </NavLink>
-        <NavLink
-            to='/create'
-            activeClassName='is-active'
-        >
-            Create Expense
-        </NavLink>
-        <button
-            onClick={startLogOutProcess}
-        >
-            Logout
-        </button>
+    <header className='header'>
+        <div className='content-container'>
+            <div className='header__content'>
+                <Link
+                    className='header__title'
+                    to='/dashboard'
+                    activeClassName='is-active'
+                    exact
+                >
+                    <h1>Expensify</h1>
+                </Link>
+                <button
+                    onClick={startLogOutProcess}
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
     </header>
 )
 
